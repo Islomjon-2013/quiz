28bac47f97e6071fede0d70ece3ad1c1cedad0b7
hulkar <hulkar@gmail.com>
2022-12-08 20:20:06 -0500

    quizess

---
+++ main.js
@@ -0,0 +1,40 @@
+let quizs = [
+    [`1-savol: dunyo so'zining englizcha nomi`,`world`],
+    [`2-savol: rang so'zining englizcha nomi`,`color`],
+    [`3-savol: raqam so'zining englizcha nomi`,`number`],
+    [`4-savol: ism so'zining englizcha nomi`,`name`],
+    [`5-savol: bolalar so'zining englizcha nomi`,`children`],
+    [`6-savol: qizil so'zining englizcha nomi`,`red`],
+];
+
+
+let result = document.getElementById(`result`);
+let k=1
+for(let quiz of quizs){
+    let card= document.createElement(`div`);
+    let savol =document.createElement(`p`)
+    let input = document.createElement(`input`);
+    savol.innerHTML = quiz[0];
+    input.setAttribute(`type`,'text');
+    input.setAttribute('placeholder',k+'-savolga javob yozing');
+    card.append(savol,input);
+    result.append(card);
+    k++;
+}
+function res(){
+    let answer = document.querySelectorAll(`input`);
+    for (i=0; i<answer.length; i++){
+        if(answer[i].value == quizs[i][1]){
+            answer[i].classList.add(`true`);
+          
+
+        }else{
+            answer[i].classList.add(`false`);
+          
+        }
+    }
+    function reload (){
+        location.reload();
+    }
+setTimeout(reload,5000)
+}
